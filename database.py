c2c103153e4753c4a196704a62532833d73233a5
Bart Piasek <bartpiasek@icloud.com>
2020-10-30 20:42:39 +0100

    database menu

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 import datetime
 import sqlite3
 
-CREATE_MOVIES_TABLE = """CREATE TABLE IF NOT EXIST movies (
+CREATE_MOVIES_TABLE = """CREATE TABLE IF NOT EXISTS movies (
     title TEXT,
     release REAL,
     watched INTEGER
@@ -12,23 +12,39 @@ INSERT_MOVIES = "INSERT INTO movies VALUES (?, ?, 0);"
 SELECT_ALL_MOVIES = "SELECT * FROM movies;"
 SELECT_UPCOMING_MOVIES = "SELECT * FROM movies WHERE release > ?;"
 SELECT_WATCHED_MOVIES = "SELECT * FROM movies WHERE watched = 1;"
+SET_MOVIE_WATCHED = "UPDATE movies SET watched = 1 WHERE title = ?;"
+DELETE_MOVIE = "DELETE FROM movies WHERE title = ?;"
 
 connection = sqlite3.connect("data.db")
 
 def create_tables():
-    pass
+    with connection:
+        connection.execute(CREATE_MOVIES_TABLE)
 
 
 def add_movie(title, release):
-    pass
+    with connection:
+        connection.execute(INSERT_MOVIES, (title, release))
 
 
 def get_movies(upcoming=False):
-    pass
+    with connection:
+        cursor = connection.cursor()
+        if upcoming:
+            today_timestamp = datetime.datetime.today().timestamp()
+            cursor.execute(SELECT_UPCOMING_MOVIES, (today_timestamp,))
+        else:
+            cursor.execute(SELECT_ALL_MOVIES)
+        return cursor.fetchall()
 
 
 def watch_movie(title):
-    pass
+    with connection:
+        connection.execute(SET_MOVIE_WATCHED, (title,))
+        
 
 def get_watched_movies():
-    pass
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(SELECT_WATCHED_MOVIES)
+        return cursor.fetchall()
